96512ac382d54656fe46c416059f4195b1f93e36
Brendon Henning <brendonhenning@gmail.com>
2020-10-12 11:38:29 -0500

    added users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    registrations: 'registrations'
+  }
+
+  resources :users, only: [:index, :show]
+
+  devise_scope :user do
+    root to: 'users#index'
+  end
 end
