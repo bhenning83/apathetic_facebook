016284d321269b107377100bbe7a6c87c0c63dd4
Brendon Henning <brendonhenning@gmail.com>
2020-10-20 12:17:59 -0500

    added profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramiters, if: :devise_controller?
+  # before_action :masquerade_user!
+
+  protected
+
+  def configure_permitted_paramiters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_one_attached :avatar
+
 end
