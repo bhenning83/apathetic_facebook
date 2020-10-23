46849a4ae568dbea55214c88dcf3b2f5163818e1
Brendon Henning <brendonhenning@gmail.com>
2020-10-23 14:26:26 -0500

    current_user's posts now appear on index page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      puts 'made it here'
       UserMailer.with(user: @user).welcome_email.deliver_now
     end
   end
